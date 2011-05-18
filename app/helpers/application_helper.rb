399cc0f4bf0ef62b22a9080945532828f3ac9354
hlnbee <helen.burgess@staff.central.wa.edu.au>
2011-05-18 20:52:12 +0800

    Removed default Rails page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Return a title on a per-page basis.                       # Documentation Comment
+  def title                                                   # Method definition
+    base_title = "Ruby on Rails Tutorial Sample App"          # Variable assignment
+    if @title.nil?                                            # boolean test for nil
+      base_title                                              # implicit return
+    else
+      "#{base_title} | #{@title}"                             # string interpolation
+    end
+  end
 end
+
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,43 +3,49 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
-  describe "GET 'home'" do
+  before(:each) do
+    #
+    # Define @base_title here.
+    #
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
 
+  describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+    
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
+                                    :content => @base_title + " | Home")
     end
   end
 
   describe "GET 'contact'" do
-
     it "should be successful" do
       get 'contact'
       response.should be_success
     end
+
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Contact")
+                                    :content => @base_title + " | Contact")
     end
   end
 
   describe "GET 'about'" do
- 
     it "should be successful" do
       get 'about'
       response.should be_success
     end
+
     it "should have the right title" do
-       get 'about'
-       response.should have_selector("title",
-                         :content => "Ruby on Rails Tutorial Sample App | About")
-     end
+      get 'about'
+      response.should have_selector("title",
+                                    :content => @base_title + " | About")
+    end
   end
 end
-
